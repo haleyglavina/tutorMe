576592e81d976c70a6a9e13bb148070426a1cc9f
Haley Glavina <haleyglav@gmail.com>
2020-09-09 17:02:28 -0400

    Create server that returns a student object and their lessons

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const {v4: uuid} = require('uuid');
+require('dotenv').config();
+const PORT = process.env.PORT || 8080;
+
+const student1 = require('./data/student1.json');
+const students = [student1];
+
+// Start server
+app.listen(PORT, () => console.log(`Server listening on ${PORT}...`));
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Handle requests
+
+// Get all lessons for a student
+app.get('/allLessons/:studentId', (req, res) => {
+  students.forEach(student => {
+    console.log(req.params.studentId)
+    if (student.id.toString() === req.params.studentId.toString())
+      return res.status(200).json(student1);
+  })
+  return res.status(400).json("No student found.");
+});
