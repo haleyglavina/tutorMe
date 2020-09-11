b8e01a19379e4f6fe25d038e36b4ff04d8d63cdb
Haley Glavina <haleyglav@gmail.com>
2020-09-11 16:36:43 -0400

    Save new lesson contents to server, displays on browseDocuments page. Only works for text elements

--- server/server.js
+++ server/server.js
@@ -5,8 +5,7 @@ const {v4: uuid} = require('uuid');
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 
-const student1 = require('./data/student1.json');
-const students = [student1];
+const students = require('./data/students.json');
 
 // Start server
 app.listen(PORT, () => console.log(`Server listening on ${PORT}...`));
@@ -20,9 +19,8 @@ app.use(express.json());
 // Get all lessons for a student
 app.get('/allLessons/:studentId', (req, res) => {
   students.forEach(student => {
-    console.log(req.params.studentId)
     if (student.id.toString() === req.params.studentId.toString()){
-      res.status(200).json(student1);
+      res.status(200).json(student);
       return;
   }});
   //res.status(400).json("No student found.");
@@ -31,7 +29,6 @@ app.get('/allLessons/:studentId', (req, res) => {
 // Get specific lesson for a student
 app.get('/lesson/:studentId/:id', (req, res) => {
   students.forEach(student => {
-    console.log(req.params.studentId)
     if (student.id.toString() === req.params.studentId.toString()) {
       student.lessons.forEach(lesson => {
         if (lesson.id.toString() === req.params.id.toString()) {
@@ -42,4 +39,21 @@ app.get('/lesson/:studentId/:id', (req, res) => {
     }
   });
   //res.status(400).json("No lesson found.");
-})
+});
+
+// Post a new lesson for a student
+app.post('/lesson/:studentId', (req, res) => {
+  console.log(req.body.elementLi);
+  res.status(200).json("ok");
+  students.forEach(student => {
+    // Find student with this id
+    if (student.id.toString() === req.params.studentId.toString()) {
+      // Add new lesson to its list of lessons
+      student.lessons = student.lessons.concat({
+        id: student.lessons[student.lessons.length - 1].id + 1,
+        elementLi: req.body.elementLi
+      });
+      return res.status(200).json("Added new lesson to memory");
+  }});
+  //res.status(400).json("Failed to save lesson");
+});
